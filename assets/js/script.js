7543ef520f02e1e668ef661a61fb966d7fd801ef
Aaditi <aadi2007.aaditi@gmail.com>
2020-08-02 14:08:20 -0700

    add tasks using callback on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 var ulEl = document.querySelector("#tasks-to-do"); 
 
 
 var createTaskHandler = function(){
+    event.preventDefault();
     
     var liEl = document.createElement("li");
-    liEl.textContent = "Adding another task";
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var taskInfoEl = document.createElement("div");
+    // give it a class name 
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     liEl.className = "task-item";
+    liEl.appendChild(taskInfoEl);
     ulEl.appendChild(liEl); 
 }
 
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
 
 
